41e1744669ff922059ffba77b5bf2e43ac1e0bd2
pllaurado <pol.llaurado@gmail.com>
2018-10-22 13:49:48 +0100

    DIT-242 : add test for filtering

--- test/unit/uk/gov/hmrc/bindingtariffclassification/repository/CaseRepositorySpec.scala
+++ test/unit/uk/gov/hmrc/bindingtariffclassification/repository/CaseRepositorySpec.scala
@@ -26,6 +26,7 @@ import reactivemongo.core.errors.DatabaseException
 import reactivemongo.play.json.ImplicitBSONHandlers._
 import uk.gov.hmrc.bindingtariffclassification.model._
 import uk.gov.hmrc.bindingtariffclassification.model.JsonFormatters.formatCase
+import uk.gov.hmrc.bindingtariffclassification.model.search.SearchCase
 import uk.gov.hmrc.bindingtariffclassification.repository.{BaseMongoIndexSpec, CaseMongoRepository, MongoDbProvider}
 import uk.gov.hmrc.bindingtariffclassification.todelete.CaseData._
 import uk.gov.hmrc.mongo.MongoSpecSupport
@@ -133,6 +134,90 @@ class CaseRepositorySpec extends BaseMongoIndexSpec
     }
   }
 
+  "get filtering by Queue" should {
+
+    val queueIdX = Some("queue_x")
+    val queueIdY = Some("queue_y")
+    val unknownQueueId = Some("unknown_queue_id")
+
+    val caseWithEmptyQueue = createCase()
+    val caseWithQueueX1 = createCase().copy(queueId = queueIdX)
+    val caseWithQueueX2 = createCase().copy(queueId = queueIdX)
+    val caseWithQueueY = createCase().copy(queueId = queueIdY)
+
+    "get by filtering on queueId with no matches should return empty sequence" in {
+
+      store(caseWithEmptyQueue, caseWithQueueX1)
+      await(repository.get(Some(SearchCase(queueId = unknownQueueId)))) shouldBe Seq.empty
+    }
+
+    "get by filtering on queueId with one match should return just this queue" in {
+
+      store(caseWithEmptyQueue, caseWithQueueX1, caseWithQueueY)
+      await(repository.get(Some(SearchCase(queueId = queueIdX)))) shouldBe Seq(caseWithQueueX1)
+    }
+
+    "get by filtering on queueId with two matches should return just those queues" in {
+      store(caseWithEmptyQueue, caseWithQueueX1, caseWithQueueX2, caseWithQueueY)
+      await(repository.get(Some(SearchCase(queueId = queueIdX)))) shouldBe Seq(caseWithQueueX1, caseWithQueueX2)
+    }
+
+  }
+
+  "get filtering by AssigneeId" should {
+
+    val assigneeX = Some("assignee_x")
+    val assigneeY = Some("assignee_y")
+    val unknownAssignee = Some("unknown_assignee_id")
+
+    val caseWithEmptyAssignee = createCase()
+    val caseWithAssigneeX1 = createCase().copy(assigneeId = assigneeX)
+    val caseWithAssigneeX2 = createCase().copy(assigneeId = assigneeX)
+    val caseWithAssigneeY1 = createCase().copy(assigneeId = assigneeY)
+
+    "get by filtering on assignee with no matches should return empty sequence" in {
+
+      store(caseWithEmptyAssignee, caseWithAssigneeX1)
+      await(repository.get(Some(SearchCase(assigneeId = unknownAssignee)))) shouldBe Seq.empty
+    }
+
+    "get by filtering on assignee with one match should return just this one" in {
+
+      store(caseWithEmptyAssignee, caseWithAssigneeX1, caseWithAssigneeY1)
+      await(repository.get(Some(SearchCase(assigneeId = assigneeX)))) shouldBe Seq(caseWithAssigneeX1)
+    }
+
+    "get by filtering on assignee with two matches should return just those two" in {
+      store(caseWithEmptyAssignee, caseWithAssigneeX1, caseWithAssigneeX2, caseWithAssigneeY1)
+      await(repository.get(Some(SearchCase(assigneeId = assigneeX)))) shouldBe Seq(caseWithAssigneeX1, caseWithAssigneeX2)
+    }
+
+  }
+
+
+  "get filtering by QueueId and AssigneeId" should {
+
+    val assigneeX = Some("assignee_x")
+    val assigneeY = Some("assignee_y")
+    val queueIdX = Some("queue_x")
+    val queueIdY = Some("queue_y")
+
+    val caseWithNoQueueAndNoAssignee = createCase()
+    val caseWithQxAndAx = createCase().copy(queueId = queueIdX, assigneeId = assigneeX)
+    val caseWithQxAndAy = createCase().copy(queueId = queueIdX, assigneeId = assigneeY)
+    val caseWithQyAndAx = createCase().copy(queueId = queueIdY, assigneeId = assigneeX)
+
+
+    "get by filtering on assignee and queue with one matches should return just this one" in {
+      store(caseWithNoQueueAndNoAssignee, caseWithQxAndAx, caseWithQxAndAy, caseWithQyAndAx)
+      await(repository.get(Some(SearchCase(queueId = queueIdX, assigneeId = assigneeX)))) shouldBe Seq(caseWithQxAndAx)
+    }
+
+  }
+
+  private def store(cases: Case*): Unit = {
+    cases.foreach { c: Case => await(repository.insert(c)) }
+  }
 
   "getByReference" should {
 
