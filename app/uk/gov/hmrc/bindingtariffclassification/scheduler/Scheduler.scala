89d54ce33224d20f4ce61f47ee862ce66dbfd1ad
Stefano Sgorlon <stefano.sgorlon@digital.hmrc.gov.uk>
2019-01-03 13:46:43 +0000

    DIT-458: tmp code to change

--- app/uk/gov/hmrc/bindingtariffclassification/scheduler/Scheduler.scala
+++ app/uk/gov/hmrc/bindingtariffclassification/scheduler/Scheduler.scala
@@ -16,82 +16,68 @@
 
 package uk.gov.hmrc.bindingtariffclassification.scheduler
 
-import java.time.{Instant, LocalDate, LocalDateTime, LocalTime}
+import java.time.Instant
 import java.time.temporal.ChronoUnit
 import java.util.concurrent.TimeUnit
 
 import akka.actor.ActorSystem
-import javax.inject._
+import javax.inject.Inject
 import play.api.Logger
 import uk.gov.hmrc.bindingtariffclassification.config.AppConfig
 import uk.gov.hmrc.bindingtariffclassification.model.SchedulerRunEvent
 import uk.gov.hmrc.bindingtariffclassification.repository.SchedulerLockRepository
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.Future.successful
 import scala.concurrent.duration.FiniteDuration
 
-@Singleton
-class Scheduler @Inject()(actorSystem: ActorSystem, appConfig: AppConfig, schedulerLockRepository: SchedulerLockRepository, job: ScheduledJob) {
+class Scheduler @Inject()(actorSystem: ActorSystem,
+                          appConfig: AppConfig,
+                          schedulerLockRepository: SchedulerLockRepository,
+                          schedulerDateUtil: SchedulerDateUtil,
+                          job: ScheduledJob) {
 
   Logger.info(s"Scheduling job [${job.name}] to run periodically at [${job.firstRunTime}] with interval [${job.interval.length} ${job.interval.unit}]")
-  actorSystem.scheduler.schedule(durationUntil(nextDateWithTime(job.firstRunTime)), job.interval, new Runnable() {
-
+  actorSystem.scheduler.schedule(durationUntil(nextRunDate), job.interval, new Runnable() {
     override def run(): Unit = {
-
-      val event = SchedulerRunEvent(job.name, expectedRunDate)
-
-      Logger.info(s"Scheduled Job [${job.name}]: Acquiring Lock")
-      schedulerLockRepository.lock(event).map {
-        case true =>
-          Logger.info(s"Scheduled Job [${job.name}]: Successfully acquired lock, Starting Job.")
-          job.execute().map { _ =>
-            Logger.info(s"Scheduled Job [${job.name}]: Completed Successfully")
-          } recover {
-            case t: Throwable => Logger.error(s"Scheduled Job [${job.name}]: Failed", t)
-          }
-        case false => Logger.info(s"Scheduled Job [${job.name}]: Failed to acquire Lock. It may be running elsewhere.")
-      }.map( _ => ())
+      execute()
     }
   })
 
-  private def expectedRunDate: Instant = {
-    /*
-    Calculates the instant in time the job theoretically SHOULD run at.
-    This needs to be consistent among multiple instances of the service (for locking) and is not necessarily the time
-    the job ACTUALLY runs at (depending on how accurate the scheduler timer is).
-    */
-    val now = Instant.now(appConfig.clock)
-
-    val firstRunDate = nextDateWithTime(job.firstRunTime)
-
-    if (firstRunDate.isBefore(now)) {
-      val intervals: Long = Math.floor((now.toEpochMilli - firstRunDate.toEpochMilli) / job.interval.toMillis).toLong
-      firstRunDate.plusMillis(intervals * job.interval.toMillis)
-    } else {
-      firstRunDate
+  def execute(): Future[Boolean] = {
+    val event = SchedulerRunEvent(job.name, closestRunDate)
+    Logger.info(s"Scheduled Job [${job.name}]: Acquiring Lock")
+    schedulerLockRepository.lock(event).flatMap {
+      case true =>
+        Logger.info(s"Scheduled Job [${job.name}]: Successfully acquired lock. Starting Job.")
+        job.execute().map { _ =>
+          Logger.info(s"Scheduled Job [${job.name}]: Completed Successfully")
+          true
+        } recover {
+          case t: Throwable =>
+            Logger.error(s"Scheduled Job [${job.name}]: Failed", t)
+            false
+        }
+      case false =>
+        Logger.info(s"Scheduled Job [${job.name}]: Failed to acquire Lock. It may have been running already.")
+        successful(false)
     }
   }
 
-  private def nextDateWithTime(nextRunTime: LocalTime): Instant = {
-    val currentDateTime = LocalDateTime.now(appConfig.clock)
-    val currentTime = currentDateTime.toLocalTime
-
-    def nextRunDateTime: LocalDateTime = {
-      if (nextRunTime.isBefore(currentTime)) {
-        nextRunTime.atDate(LocalDate.now(appConfig.clock).plusDays(1))
-      } else {
-        nextRunTime.atDate(LocalDate.now(appConfig.clock))
-      }
-    }
-
-    nextRunDateTime.atZone(appConfig.clock.getZone).toInstant
+  private def nextRunDate: Instant = {
+    schedulerDateUtil.nextRun(job.firstRunTime, job.interval)
   }
 
-  private def durationUntil(date: Instant): FiniteDuration = {
+  private def closestRunDate: Instant = {
+    schedulerDateUtil.closestRun(job.firstRunTime, job.interval)
+  }
+
+  private def durationUntil(datetime: Instant): FiniteDuration = {
     val now = Instant.now(appConfig.clock)
 
-    if (now.isBefore(date)) FiniteDuration(now.until(date, ChronoUnit.SECONDS), TimeUnit.SECONDS)
-    else FiniteDuration(0, TimeUnit.SECONDS)
+    if (datetime.isBefore(now)) throw new IllegalArgumentException(s"Expected a future or present datetime but was [$datetime]")
+    else FiniteDuration(now.until(datetime, ChronoUnit.SECONDS), TimeUnit.SECONDS)
   }
 
 }
