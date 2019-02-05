061a1b7797b1d99e344c5541e5a803a478cc5cc8
Stefano Sgorlon <stefano.sgorlon@digital.hmrc.gov.uk>
2019-02-05 16:59:08 +0000

    BAU: cleaning

--- app/uk/gov/hmrc/bindingtariffclassification/sort/SortField.scala
+++ app/uk/gov/hmrc/bindingtariffclassification/sort/SortField.scala
@@ -14,10 +14,12 @@
  * limitations under the License.
  */
 
-package uk.gov.hmrc.bindingtariffclassification.model.sort
+package uk.gov.hmrc.bindingtariffclassification.sort
 
-object SortDirection extends Enumeration {
-  type SortDirection = Value
-  val DESCENDING = Value(-1, "desc")
-  val ASCENDING = Value(1, "asc")
+
+object SortField extends Enumeration {
+  type SortField = Value
+
+  val DAYS_ELAPSED = Value("days-elapsed")
+  val COMMODITY_CODE = Value("commodity-code")
 }
