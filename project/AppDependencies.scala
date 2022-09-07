7e4d450039970fa0f1680ce49fdea7c4c95d1fab
nboaram <14924378+Nboaram@users.noreply.github.com>
2022-09-07 10:53:05 +0100

    [DDCE-2637] Scalastyle and Scalafmt in build.sbt, bootstrap to 7.2.0 and gitignore

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,37 +4,37 @@ import play.core.PlayVersion.current
 
 object AppDependencies {
   private lazy val mongoHmrcVersion = "0.71.0"
-  val AkkaVersion = "2.6.19"
-  private val silencerVersion = "1.7.9"
-  val jettyVersion = "9.4.48.v20220622"
+  val AkkaVersion                   = "2.6.19"
+  private val silencerVersion       = "1.7.9"
+  val jettyVersion                  = "9.4.48.v20220622"
 
   private val silencerDependencies: Seq[ModuleID] = Seq(
     compilerPlugin("com.github.ghik" % "silencer-plugin" % silencerVersion cross CrossVersion.full),
     "com.github.ghik" % "silencer-lib" % silencerVersion % Provided cross CrossVersion.full
   )
 
-  lazy val compile = Seq(
-    "uk.gov.hmrc"          %% "bootstrap-backend-play-28"     % "7.1.0",
-    "uk.gov.hmrc"          %% "crypto"                        % "7.1.0",
-    "uk.gov.hmrc"          %% "play-json-union-formatter"     % "1.15.0-play-28",
-    "uk.gov.hmrc.mongo"    %% "hmrc-mongo-play-28"            % mongoHmrcVersion,
-    "com.lightbend.akka"   %% "akka-stream-alpakka-mongodb"   % "3.0.4",
-    "com.typesafe.akka"    %% "akka-stream"                   % AkkaVersion,
-    "com.typesafe.play"    %% "play-json"                     % "2.9.3",
-    "org.typelevel"        %% "cats-core"                     % "2.8.0",
-    "com.github.pathikrit" %% "better-files"                  % "3.9.1",
-    "org.quartz-scheduler" % "quartz"                         % "2.3.2",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.13.4",
+  lazy val compile: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"                  %% "bootstrap-backend-play-28"   % "7.2.0",
+    "uk.gov.hmrc"                  %% "crypto"                      % "7.1.0",
+    "uk.gov.hmrc"                  %% "play-json-union-formatter"   % "1.15.0-play-28",
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"          % mongoHmrcVersion,
+    "com.lightbend.akka"           %% "akka-stream-alpakka-mongodb" % "3.0.4",
+    "com.typesafe.akka"            %% "akka-stream"                 % AkkaVersion,
+    "com.typesafe.play"            %% "play-json"                   % "2.9.3",
+    "org.typelevel"                %% "cats-core"                   % "2.8.0",
+    "com.github.pathikrit"         %% "better-files"                % "3.9.1",
+    "org.quartz-scheduler"         % "quartz"                       % "2.3.2",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"        % "2.13.4",
     ws
   )
 
   val test: Seq[ModuleID] = Seq(
     "com.github.tomakehurst" % "wiremock"                  % "2.33.2",
-    "com.typesafe.play"      %% "play-test"                % current ,
+    "com.typesafe.play"      %% "play-test"                % current,
     "org.mockito"            % "mockito-core"              % "4.7.0",
     "org.jsoup"              % "jsoup"                     % "1.15.3",
-    "org.scalatest"          %% "scalatest"                % "3.2.13" ,
-    "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0" ,
+    "org.scalatest"          %% "scalatest"                % "3.2.13",
+    "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0",
     "org.scalatestplus"      %% "mockito-3-4"              % "3.2.10.0",
     "com.vladsch.flexmark"   % "flexmark-all"              % "0.62.2",
     "org.scalacheck"         %% "scalacheck"               % "1.16.0",
