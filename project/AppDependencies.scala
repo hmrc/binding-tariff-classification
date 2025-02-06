56b254cd2a717dc4f1ccd356856dc6138e75b8ee
SachaMcCormack <175815565+SachaMcCormack@users.noreply.github.com>
2025-02-06 14:39:15 +0000

    fixed errors

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,19 +3,19 @@ import sbt.*
 object AppDependencies {
   private lazy val mongoHmrcVersion = "2.4.0"
   private lazy val bootstrapVersion = "9.8.0"
-  private lazy val pekkoVersion     = "1.1.0"
+  private lazy val pekkoVersion     = "1.1.3"
 
   lazy val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-30"   % bootstrapVersion,
     "uk.gov.hmrc"                  %% "play-json-union-formatter"   % "1.22.0",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"          % mongoHmrcVersion,
-    "org.apache.pekko"             %% "pekko-connectors-mongodb"    % pekkoVersion,
+    "org.apache.pekko"             %% "pekko-connectors-mongodb"    % "1.1.0",
     "org.apache.pekko"             %% "pekko-stream"                % pekkoVersion,
     "org.apache.pekko"             %% "pekko-actor-typed"           % pekkoVersion,
     "org.apache.pekko"             %% "pekko-serialization-jackson" % pekkoVersion,
-    "org.typelevel"                %% "cats-core"                   % "2.12.0",
+    "org.typelevel"                %% "cats-core"                   % "2.13.0",
     "com.github.pathikrit"         %% "better-files"                % "3.9.2",
-    "org.quartz-scheduler"          % "quartz"                      % "2.3.2",
+    "org.quartz-scheduler"          % "quartz"                      % "2.5.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala"        % "2.18.2"
   )
 
