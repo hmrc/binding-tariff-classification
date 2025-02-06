78e930f1a20215831df5760ef7e23fc97356aa89
SachaMcCormack <175815565+SachaMcCormack@users.noreply.github.com>
2025-02-06 14:17:59 +0000

    updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,9 +1,9 @@
 import sbt.*
 
 object AppDependencies {
-  private lazy val mongoHmrcVersion = "2.2.0"
-  private lazy val bootstrapVersion = "9.2.0"
-  private lazy val pekkoVersion     = "1.0.2"
+  private lazy val mongoHmrcVersion = "2.4.0"
+  private lazy val bootstrapVersion = "9.8.0"
+  private lazy val pekkoVersion     = "1.1.0"
 
   lazy val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-backend-play-30"   % bootstrapVersion,
@@ -16,13 +16,13 @@ object AppDependencies {
     "org.typelevel"                %% "cats-core"                   % "2.12.0",
     "com.github.pathikrit"         %% "better-files"                % "3.9.2",
     "org.quartz-scheduler"          % "quartz"                      % "2.3.2",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"        % "2.17.2"
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"        % "2.18.2"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % mongoHmrcVersion,
-    "org.apache.pekko"  %% "pekko-http-testkit"      % "1.0.1"
+    "org.apache.pekko"  %% "pekko-http-testkit"      % "1.1.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
