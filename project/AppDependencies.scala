961c3baabb07fb042f1dc491d7142312b84aa95a
divya-manivannan-hmrc <175815017+Divya-Manivannan-hmrc@users.noreply.github.com>
2025-02-24 13:05:11 +0000

    DDCE-6416 - Added tests for Union

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
   private lazy val mongoHmrcVersion = "2.5.0"
-  private lazy val bootstrapVersion = "9.8.0"
+  private lazy val bootstrapVersion = "9.9.0"
   private lazy val pekkoVersion     = "1.1.3"
 
   lazy val compile: Seq[ModuleID] = Seq(
