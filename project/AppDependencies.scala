c268e1543465c233d2861a840d1e81996f39ecf9
erickbm6922 <47341247+erickbm6922@users.noreply.github.com>
2026-03-11 09:26:56 +0000

    DDCE-8206: Updating dependecies to address vulenarabilities and also update the year in the copyright header

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,7 +1,7 @@
 import sbt.*
 
 object AppDependencies {
-  private lazy val mongoHmrcVersion = "2.11.0"
+  private lazy val mongoHmrcVersion = "2.12.0"
   private lazy val bootstrapVersion = "10.7.0"
   private lazy val pekkoVersion     = "1.1.5"
 
