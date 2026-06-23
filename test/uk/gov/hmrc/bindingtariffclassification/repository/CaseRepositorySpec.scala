a9c487bd0fca40ea3a770de9984e8501aec07ced
tolgatunaaa <107684014+tolgatunaaa@users.noreply.github.com>
2026-06-24 00:52:39 +0100

    DDCE-7702 - Test fix for index

--- test/uk/gov/hmrc/bindingtariffclassification/repository/CaseRepositorySpec.scala
+++ test/uk/gov/hmrc/bindingtariffclassification/repository/CaseRepositorySpec.scala
@@ -2511,7 +2511,14 @@ class CaseRepositorySpec
           ascending("decision.bindingCommodityCode"),
           IndexOptions().name("decision_bindingCommodityCode_Index")
         ),
-        IndexModel(ascending("keywords"), IndexOptions().name("keywords_Index"))
+        IndexModel(ascending("keywords"), IndexOptions().name("keywords_Index")),
+        IndexModel(
+          Indexes.compoundIndex(
+            Indexes.ascending("keyword"),
+            Indexes.ascending("reference")
+          ),
+          IndexOptions().name("keyword_reference_view_idx")
+        )
       )
 
       val repo = newMongoRepository
