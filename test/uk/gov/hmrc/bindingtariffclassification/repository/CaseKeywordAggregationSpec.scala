cb07f31554f38a94cfa6f3e08dc5a0e30d1653b4
tolgatunaaa <107684014+tolgatunaaa@users.noreply.github.com>
2026-06-24 01:04:17 +0100

    test fix

--- test/uk/gov/hmrc/bindingtariffclassification/repository/CaseKeywordAggregationSpec.scala
+++ test/uk/gov/hmrc/bindingtariffclassification/repository/CaseKeywordAggregationSpec.scala
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2026 HM Revenue & Customs
  *
@@ -5,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,7 +17,7 @@
 
 package uk.gov.hmrc.bindingtariffclassification.repository
 
-import org.mockito.ArgumentMatchers.any
+import org.mockito.ArgumentMatchers.{any, anyInt}
 import org.mockito.Mockito.when
 import org.mongodb.scala._
 import org.mongodb.scala.bson.conversions.Bson
@@ -26,8 +27,7 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.mockito.MockitoSugar
 import uk.gov.hmrc.bindingtariffclassification.model._
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 class CaseKeywordAggregationSpec
   extends AnyWordSpec
@@ -35,16 +35,20 @@ class CaseKeywordAggregationSpec
     with MockitoSugar
     with ScalaFutures {
 
+  implicit val ec: ExecutionContext = ExecutionContext.global
+
   private val keywordRepo = mock[KeywordsMongoRepository]
   private val viewUpdater = mock[CaseKeywordViewUpdater]
 
-  private val collection = mock[MongoCollection[CaseKeywordViewRow]]
-  private val findObs    = mock[FindObservable[CaseKeywordViewRow]]
+  private val keywordCollection = mock[MongoCollection[Keyword]]
+  private val keywordFindObs    = mock[FindObservable[Keyword]]
 
-  private val aggregationService =
-    new CaseKeywordAggregation(keywordRepo, viewUpdater)
+  private val viewCollection    = mock[MongoCollection[CaseKeywordViewRow]]
+  private val viewFindObs       = mock[FindObservable[CaseKeywordViewRow]]
+  private val countObservable   = mock[SingleObservable[java.lang.Long]]
 
-  private val pagination = Pagination()
+  private val aggregationService = new CaseKeywordAggregation(keywordRepo, viewUpdater)
+  private val pagination         = Pagination()
 
   private val rowBikeBti = CaseKeywordViewRow(
     keyword = "bike",
@@ -72,87 +76,46 @@ class CaseKeywordAggregationSpec
     liabilityStatus = Some("LIVE")
   )
 
+  private def stubMockDriver(approvedKeywords: Seq[Keyword], viewRows: Seq[CaseKeywordViewRow], totalCount: Long): Unit = {
+    when(keywordRepo.collection).thenReturn(keywordCollection)
+    when(keywordCollection.find[Keyword](any[Bson]())(any(), any())).thenReturn(keywordFindObs)
+    when(keywordFindObs.toFuture()).thenReturn(Future.successful(approvedKeywords))
+
+    when(viewUpdater.collection).thenReturn(viewCollection)
+
+    when(viewCollection.countDocuments(any[Bson]())(any(), any())).thenReturn(countObservable)
+    when(countObservable.toFuture()).thenReturn(Future.successful(java.lang.Long.valueOf(totalCount)))
+
+    when(viewCollection.find[CaseKeywordViewRow](any[Bson]())(any(), any())).thenReturn(viewFindObs)
+    when(viewFindObs.sort(any[Bson]())).thenReturn(viewFindObs)
+    when(viewFindObs.skip(anyInt())).thenReturn(viewFindObs)
+    when(viewFindObs.limit(anyInt())).thenReturn(viewFindObs)
+    when(viewFindObs.toFuture()).thenReturn(Future.successful(viewRows))
+  }
+
   "CaseKeywordAggregation" should {
 
     "fetchKeywordsFromCases should return mapped CaseKeywords from the materialized view" in {
+      stubMockDriver(
+        approvedKeywords = Seq.empty,
+        viewRows = Seq(rowBikeBti, rowToolLiability),
+        totalCount = 2
+      )
 
-      when(keywordRepo.approvedKeywords())
-        .thenReturn(Future.successful(Seq.empty))
-
-      when(viewUpdater.collection)
-        .thenReturn(collection)
-
-      when(collection.countDocuments(any[Bson]))
-        .thenReturn(SingleObservable(2L))
-
-      when(collection.find(any[Bson]))
-        .thenReturn(findObs)
-
-      when(findObs.sort(any[Bson]))
-        .thenReturn(findObs)
-
-      when(findObs.skip(any[Int]))
-        .thenReturn(findObs)
-
-      when(findObs.limit(any[Int]))
-        .thenReturn(findObs)
-
-      when(findObs.toFuture())
-        .thenReturn(
-          Future.successful(
-            Seq(rowBikeBti, rowToolLiability)
-          )
-        )
-
-      val result =
-        aggregationService
-          .fetchKeywordsFromCases(pagination)
-          .futureValue
+      val result = aggregationService.fetchKeywordsFromCases(pagination).futureValue
 
       result.resultCount shouldBe 2
-
-      result.results.map(_.keyword.name) should contain theSameElementsAs
-        Seq("bike", "tool")
+      result.results.map(_.keyword.name) should contain theSameElementsAs Seq("bike", "tool")
     }
 
     "fetchKeywordsFromCases should exclude approved keywords" in {
+      stubMockDriver(
+        approvedKeywords = Seq(Keyword(name = "tool", approved = true)),
+        viewRows = Seq(rowBikeBti),
+        totalCount = 1
+      )
 
-      when(keywordRepo.approvedKeywords())
-        .thenReturn(
-          Future.successful(
-            Seq(Keyword("tool", approved = true))
-          )
-        )
-
-      when(viewUpdater.collection)
-        .thenReturn(collection)
-
-      when(collection.countDocuments(any[Bson]))
-        .thenReturn(SingleObservable(1L))
-
-      when(collection.find(any[Bson]))
-        .thenReturn(findObs)
-
-      when(findObs.sort(any[Bson]))
-        .thenReturn(findObs)
-
-      when(findObs.skip(any[Int]))
-        .thenReturn(findObs)
-
-      when(findObs.limit(any[Int]))
-        .thenReturn(findObs)
-
-      when(findObs.toFuture())
-        .thenReturn(
-          Future.successful(
-            Seq(rowBikeBti)
-          )
-        )
-
-      val result =
-        aggregationService
-          .fetchKeywordsFromCases(pagination)
-          .futureValue
+      val result = aggregationService.fetchKeywordsFromCases(pagination).futureValue
 
       result.resultCount shouldBe 1
       result.results.map(_.keyword.name) shouldBe Seq("bike")
